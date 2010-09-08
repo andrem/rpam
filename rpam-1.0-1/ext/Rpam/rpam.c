4ab56335efcecf108067ff327fa5f87d462bccd2
Larry Reaves <larry@yrral.net>
2010-09-08 12:47:38 -0700

    Fix STR2CSTR uses, which are deprecated in ruby 1.8 and removed in ruby 1.9

--- rpam-1.0-1/ext/Rpam/rpam.c
+++ rpam-1.0-1/ext/Rpam/rpam.c
@@ -95,8 +95,8 @@ VALUE method_authpam(VALUE self, VALUE username, VALUE password) {
 	pam_handle_t *pamh = NULL;
 	int result;
 
-   	userinfo.name = STR2CSTR(username);
-	userinfo.pw =   STR2CSTR(password);
+   	userinfo.name = StringValuePtr(username);
+	userinfo.pw =   StringValuePtr(password);
  
 	if ((result = pam_start(rpam_servicename, userinfo.name, &conv_info, &pamh)) 
             != PAM_SUCCESS) {
